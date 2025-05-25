33bc23f2ab32b413402167817716a6ebe46287f8
baiyuze <1347417308@qq.com>
2025-05-26 01:40:03 +0800

    update

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,21 +1,21 @@
 package model
 
 import (
-		"time"
+	"time"
 )
 
 type User struct {
-		ID         int       `gorm:"primaryKey;autoIncrement" json:"id"`
-		Name       string    `gorm:"type:char(30);not null" json:"name"`
-		Account    string    `gorm:"type:char(30);not null" json:"account"`
-		CreateTime time.Time `gorm:"type:datetime(6);" json:"create_time"`
-		Password   *string   `gorm:"type:varchar(255);not null" json:"password,omitempty"`
-		Roles      []*Roles  `gorm:"many2many:user_roles"`
-		Phone      string    `gorm:"type:char(30)" json:"phone"`
-		Email      string    `gorm:"type:char(100)" json:"email"`
-		UpdateTime time.Time `gorm:"type:datetime(6);autoUpdateTime" json:"update_time"`
+	ID         int       `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name       string    `gorm:"type:char(30);not null" json:"name"`
+	Account    string    `gorm:"type:char(30);not null" json:"account"`
+	CreateTime time.Time `gorm:"type:datetime(6);" json:"create_time"`
+	Password   *string   `gorm:"type:varchar(255);not null" json:"password,omitempty"`
+	Roles      []*Roles  `gorm:"many2many:user_roles"`
+	Phone      string    `gorm:"type:char(30)" json:"phone"`
+	Email      string    `gorm:"type:char(100)" json:"email"`
+	UpdateTime time.Time `gorm:"type:datetime(6);autoUpdateTime" json:"update_time"`
 }
 
 func (User) TableName() string {
-		return "user"
+	return "user"
 }
